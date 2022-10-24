0fb4677bda4a13d061da8beced75f299f18df976
Ховалыг Тараа Эресович <TEKhovalyg@sberbank.ru>
2022-10-24 20:23:43 +0300

    global values added to Vagrantfile of p1. Also added golang compiler installing.

--- p1/scripts/script.go
+++ p1/scripts/script.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 )
+
+func addPublicKey(publicKey string) {
+	exec.Command("cat", publicKey, ">>", "~/.ssh/authorized_keys")
+}
 
 func main() {
 	fmt.Println("Hello vagrant!")
+	addPublicKey(os.Args[1])
 }
