aadb3ed918fd4bb4ed40377f9363e5991318c25e
jkeitha <jkeitha@student.21-school.ru>
2022-11-12 01:08:57 +0300

    modified:   p1/scripts/install.go, add ToDo

--- p1/scripts/install.go
+++ p1/scripts/install.go
@@ -51,6 +51,7 @@ func NewDataStore(args []string) *DataStore {
 
 func main() {
 	fmt.Println("Install script running!")
+	// ToDO: 1. В скрипт добавить установку vim, k8s;
 	//dataStore := NewDataStore(os.Args)
 	//addPublicKey(dataStore.sshPublicKeyPath)
 }
